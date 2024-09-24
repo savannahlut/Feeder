7279cdd082cd6b8e6bec232cd7f38889a92c587e
savannahlut <savannahlut@nycstudents.net>
2024-09-24 19:33:05 +0000

    Tested Examples 1

--- Main.java
+++ Main.java
@@ -4,5 +4,9 @@ public class Main
     {
         Feeder j = new Feeder(0); 
         System.out.println(j.simulateManyDays(5, 10));
+        Feeder g = new Feeder(250); 
+        System.out.println(g.simulateManyDays(10, 5));
+        Feeder i = new Feeder(2400); 
+        System.out.println(i.simulateManyDays(10, 4));
     }
 }
