7f175c59c44b7311f8259ed5b6720e983669e5cb
savannahlut <savannahlut@nycstudents.net>
2024-09-25 13:13:33 +0000

    Tested Examples 2

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@ public class Main
 {
     public static void main (String[] args)
     {
+        Feeder a = new Feeder(500);
+        a.simulateOneDay(12);
+        System.out.println(a);
+        Feeder b = new Feeder(1000);
+        b.simulateOneDay(22);
+        System.out.println(b);
+        Feeder c = new Feeder(100);
+        c.simulateOneDay(5);
+        System.out.println(c);
+
         Feeder j = new Feeder(0); 
         System.out.println(j.simulateManyDays(5, 10));
         Feeder g = new Feeder(250); 
